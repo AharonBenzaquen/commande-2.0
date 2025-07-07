137eb4eec3abbdda912892c341b5ac43b77e559d
AharonBenzaquen <aharon.b@opti-w.com>
2025-07-07 13:07:32 -0400

    Update OuNousTrouver.js

--- src/OuNousTrouver.js
+++ src/OuNousTrouver.js
@@ -8,7 +8,7 @@ export default function OuNousTrouver() {
 
       <div className="magasin-bloc">
         <h3>Opti-W Laval</h3>
-        <p>Adresse : 123 Rue Laval, QC</p>
+        <p>Adresse : 2075 Bd Chomedey #110, Laval, Quebec H7T 0G5, QC</p>
         <p>Téléphone : (514) 123-4567</p>
         <button className="maps-link-button" onClick={() => window.open('https://www.google.com/maps?q=opti-w+laval', '_blank')}>
           Voir sur Google Maps
