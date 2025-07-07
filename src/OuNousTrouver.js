e77165f3a2e3633463829fa30c530feb2dda12f3
AharonBenzaquen <aharon.b@opti-w.com>
2025-07-07 19:58:03 -0400

    Update OuNousTrouver.js

--- src/OuNousTrouver.js
+++ src/OuNousTrouver.js
@@ -4,31 +4,44 @@ import './index.css';
 export default function OuNousTrouver() {
   return (
     <div className="app">
+      {/* 🧭 Titre principal */}
       <h2 className="header">📍 Nos Magasins</h2>
 
+      {/* 🏬 Magasin Laval */}
       <div className="magasin-bloc">
         <h3>Opti-W Laval</h3>
-        <p>Adresse : 2075 Bd Chomedey, Laval, Quebec H7T 0G5</p>
+        <p>Adresse : 2075 Bd Chomedey, Laval, Québec H7T 0G5</p>
         <p>Téléphone : (450) 687-8787</p>
-        <button className="maps-link-button" onClick={() => window.open('https://www.google.com/maps?q=opti-w+laval', '_blank')}>
+        <button
+          className="maps-link-button"
+          onClick={() => window.open('https://www.google.com/maps?q=opti-w+laval', '_blank')}
+        >
           Voir sur Google Maps
         </button>
       </div>
 
+      {/* 🏬 Magasin Rosemère */}
       <div className="magasin-bloc">
         <h3>Opti-W Rosemère</h3>
-        <p>Adresse :401 Boul. Labelle, Rosemère, QC J7A 3T2</p>
+        <p>Adresse : 401 Boul. Labelle, Rosemère, QC J7A 3T2</p>
         <p>Téléphone : (450) 951-2115</p>
-        <button className="maps-link-button" onClick={() => window.open('https://www.google.com/maps?q=opti-w+rosemère', '_blank')}>
+        <button
+          className="maps-link-button"
+          onClick={() => window.open('https://www.google.com/maps?q=opti-w+rosemère', '_blank')}
+        >
           Voir sur Google Maps
         </button>
       </div>
 
+      {/* 🏬 Magasin Blainville */}
       <div className="magasin-bloc">
         <h3>Opti-W Blainville</h3>
         <p>Adresse : 1333 Bd Michèle-Bohec, Blainville, QC J7C 5M7</p>
-        <p>Téléphone : (514) 416 1909</p>
-        <button className="maps-link-button" onClick={() => window.open('https://www.google.com/maps?q=opti-w+blainville', '_blank')}>
+        <p>Téléphone : (514) 416-1909</p>
+        <button
+          className="maps-link-button"
+          onClick={() => window.open('https://www.google.com/maps?q=opti-w+blainville', '_blank')}
+        >
           Voir sur Google Maps
         </button>
       </div>
