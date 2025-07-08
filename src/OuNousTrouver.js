cf7aec8670943e1425b83922ac0ce80adf3cf6a5
AharonBenzaquen <aharon.b@opti-w.com>
2025-07-08 18:48:18 -0400

    Update OuNousTrouver.js

--- src/OuNousTrouver.js
+++ src/OuNousTrouver.js
@@ -25,8 +25,7 @@ export default function OuNousTrouver() {
           <iframe
             title="Opti-W Laval"
             className="store-map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5587.040541631336!2d-73.75526372379899!3d45.55997522704329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc923b8d2021d6b%3A0xe3322ddc754869f5!2sOpti-W%20Laval%20(optometriste%20et%20opticien%20Walmart)!5e0!3m2!1sfr!2sca!4v1752014723813!5m2!1sfr!2sca
-"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5587.040541631336!2d-73.75526372379899!3d45.55997522704329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc923b8d2021d6b%3A0xe3322ddc754869f5!2sOpti-W%20Laval%20(optometriste%20et%20opticien%20Walmart)!5e0!3m2!1sfr!2sca!4v1752014723813!5m2!1sfr!2sca"
             allowFullScreen
             loading="lazy"
           ></iframe>
